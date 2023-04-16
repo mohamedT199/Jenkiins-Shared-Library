c5aeee714ffc891a7289ed87449838e299ed54fd
mhassan <mhassan@ntgclarity.com>
2023-04-16 12:58:42 +0200

    edit in Docker Class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,7 +10,7 @@ class Docker implements Serializable{
 
     def buildDockerImage(String credential){
         script.echo "Build Docker Image Start ...."
-        script.withCredentials([script.usernamePassword(credentialsId: $credential , usernameVariable: 'USER' , passwordVariable: 'PASS')]){
+        script.withCredentials([script.usernamePassword(credentialsId: "$credential" , usernameVariable: 'USER' , passwordVariable: 'PASS')]){
             script.sh "docker build -t talat345/demo-repo:${script.ImageName} . "
             script.sh "docker push talat345/demo-repo:${script.ImageName} "
         }
