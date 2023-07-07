007435a715a02f869687c8e9fe3c9e45e228af51
mhassan <mhassan@ntgclarity.com>
2023-07-07 18:09:43 +0300

    edit function bumb version

--- vars/bumpVersion.groovy
+++ vars/bumpVersion.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 import com.example.Maven
 def call(String pomDir){
-    if (!pomDir.isNullOrEmpty()){
+    if (!pomDir.isEmpty()){
         sh "cd $pomDir"
     }
     return new Maven(this).bumpVersion()
