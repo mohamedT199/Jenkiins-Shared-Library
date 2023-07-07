61dc628c9d994e2d9cc28379c46df6df0a2419dd
mhassan <mhassan@ntgclarity.com>
2023-07-07 18:47:29 +0300

    edit repo for comptability with Shared Jenkins Lib

--- vars/bumpVersion.groovy
+++ vars/bumpVersion.groovy
@@ -1,12 +1,5 @@
 #!/usr/bin/env groovy
 import com.example.Maven
 def call(String pomDir){
-    /*if (!pomDir.isEmpty()){
-        sh "echo $pomDir "
-        //sh "cd $pomDir"
-        sh "cd /var"
-        sh "pwd"
-    }
-    sh "pwd"*/
     return new Maven(this).bumpVersion(pomDir)
 }
