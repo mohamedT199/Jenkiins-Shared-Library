dd1d3481f8ccd4daee9845a84d0e239cce369ce5
mhassan <mhassan@ntgclarity.com>
2023-07-08 19:12:13 +0300

    edit for push new version to git on all projects Types

--- vars/buildArtifact.groovy
+++ vars/buildArtifact.groovy
@@ -2,15 +2,15 @@
 import com.example.Maven
 import com.example.NPM
 
-def call(Boolean test){
+def call(Boolean test, String branch){
     def maven = "pom.xml"
     def npm = "package.json"
     if (fileExists(maven)) {
         sh "echo 'its java '"
-        return new Maven(this).buildArtifact(test)
+        return new Maven(this).buildArtifact(test,branch)
     }else if (fileExists(npm)){
         sh "echo 'its angular '"
-        return new NPM(this).buildArtifact(test)
+        return new NPM(this).buildArtifact(test,branch)
     }
-    return new Maven(this).buildArtifact(test)
+    return new Maven(this).buildArtifact(test,branch)
 }
