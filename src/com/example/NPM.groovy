1451cebd718265e2e63acfc0fb6356a7ca621fd9
mhassan <mhassan@ntgclarity.com>
2023-07-09 17:00:52 +0300

    edit for git push after upgrade

--- src/com/example/NPM.groovy
+++ src/com/example/NPM.groovy
@@ -21,7 +21,7 @@ class NPM implements  Serializable , LanguageType {
         script.sh "git status "
         script.echo "git url ${script.env.GIT_URL}"
         script.echo "git Branch ${script.env.GIT_BRANCH}"
-        script.sh "git remote origin set-url ${script.env.GIT_URL} "
+        script.sh "git remote set-url origin  ${script.env.GIT_URL} "
         script.sh "git add package.json "
         script.sh 'git commit -m "version" '
         script.sh "git push origin HEAD:${script.env.GIT_BRANCH}"
