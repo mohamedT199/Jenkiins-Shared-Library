3ebfaa0f48d7c3ac4437f0b184f1e9bc135fbd3a
mhassan <mhassan@ntgclarity.com>
2023-07-14 18:44:36 +0300

    add Send Mail

--- src/com/example/MAIL.groovy
+++ src/com/example/MAIL.groovy
@@ -8,7 +8,7 @@ class MAIL {
     // 1- Should have Mailer Plugin
     // 2- Should Configure Mail Configuration Mail Server and user name and Password and be Tested
     // 3- once done you could use this Plugin using the below syntax
-    def sendMail(String recEmail , String body , String subject){
-        script.mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${recEmail}"
+    def sendMail(String recEmail , String ccEmail , String body , String subject){
+        script.mail bcc: '', body: "${body}", cc: "${ccEmail}", from: '', replyTo: '', subject: "${subject}", to: "${recEmail}"
     }
 }
--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import com.example.MAIL
 
-def call(String recEmail , String body , String subject){
-    return new MAIL(this).sendMail(recEmail,body,subject)
+def call(String recEmail , String ccEmail ,  String body , String subject){
+    return new MAIL(this).sendMail(recEmail,ccEmail,body,subject)
 }
